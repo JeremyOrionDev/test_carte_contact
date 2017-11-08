1618f4661e29b27d0220fc6fee8542674240c71e
jeremyorionDev <jeremy9315@hotmail.fr>
2017-11-08 09:33:59 +0100

    ajout classe Liaison serie

--- test_carte_contact/Form1.cs
+++ test_carte_contact/Form1.cs
@@ -4,17 +4,74 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Drawing;
+using System.Threading;
+using System.IO;
+using System.IO.Ports;
+
 
 namespace test_carte_contact
 {
     public partial class Form1 : Form
     {
+        private SerialPort SP = new SerialPort();
+        public enum LogMsType { Incoming, Outgoing, Normal, Warning, Error};
+        private Color[] LogMsgTypeColor = { Color.Blue, Color.Green, Color.Black, Color.Orange, Color.Red };
+
         public Form1()
         {
             InitializeComponent();
+            SP.BaudRate = 115200;
+            SP.Parity = Parity.None;
+            SP.StopBits = StopBits.One;
+            SP.DataBits = 8;
+            SP.Handshake = Handshake.None;
+            SP.DataReceived += new SerialDataReceivedEventHandler(port_DataReceived);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            bool erreur = false;
+
+            //si le port est deja ouvert on le ferme
+            if (SP.IsOpen) SP.Close();
+            else
+            {
+                //Réglages paramètres du port
+                SP.PortName = cBxPortName.Text;
+
+                try
+                {
+                    //Ouverture du port
+                    SP.Open();
+                }
+                catch (UnauthorizedAccessException) { erreur = true; }
+                catch (IOException) { erreur = true; }
+                catch (ArgumentException) { erreur = true; }
+
+                if (erreur)
+                {
+                    MessageBox.Show("Impossible d'ouvrir le por COM.\n Il est vraisemblablement déja utilisé, supprimé ou indisponible", "Port COM indisponible", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else
+                {
+                    MessageBox.Show("Connexion réussi", "Port disponible");
+                }
+            }
+
+        }
+
+        //Connexion au terminal
+        private void Log(LogMsType msgtype, string msg)
+        {
+            Console.Write(new EventHandler(delegate
+            {
+                
+            }))
         }
     }
 }
---
+++ test_carte_contact/LiaisonSerie.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+namespace test_carte_contact
+{
+    class LiaisonSerie
+    {
+        string name;
+        string Message;
+        Thread readThread;
+        bool _continue;
+        SerialPort _serialPort;
+
+        public LiaisonSerie()
+        {
+            // Création du port de communication serialport avec les différents paramètres.
+            _serialPort = new SerialPort();
+            _serialPort.PortName = "COM1";
+            _serialPort.BaudRate = 115000;//Vitesse de dialogue avec le Millenium.
+            _serialPort.Parity = Parity.Even;//Parité paire (fixé par le Millenium).
+            _serialPort.DataBits = 7;//Format de la trame 7 bits (lui aussi fixé par le Millenium).
+            _serialPort.StopBits = StopBits.One;////1 bit de Stop(fixé par le Millenium).        
+        }
+
+        public void Read()
+        {
+            byte[] buffer = new byte[100];
+            while (true)
+            {
+                try
+                {
+                    //Lecture sur le port serie.
+                    Message = _serialPort.ReadLine();
+                    Console.WriteLine(Message);
+                    //string message = _serialPort.ReadLine();
+                    //_serialPort.ReadChar();
+                }
+                catch (TimeoutException) { }
+            }
+        }
+        public void Write()
+        {
+            try
+            {
+                //Ecriture sur le port serie.  
+                _serialPort.WriteLine("test");
+                System.Windows.Forms.MessageBox.Show("Ecriture Reussi .");
+            }
+            catch (TimeoutException) { }
+
+        }
+        public string message
+        {
+            get
+            {
+                return this.Message;
+            }
+        }
+        public bool Continue
+        {
+            get
+            {
+                return this._continue;
+            }
+            set
+            {
+                _continue = value;
+            }
+        }
+        public void Open()
+        {
+            _serialPort.Open();
+        }
+        public void Close()
+        {
+            _serialPort.Close();
+        }
+        ~LiaisonSerie()
+        {
+            _serialPort.Close();
+        }
+    }
+}
